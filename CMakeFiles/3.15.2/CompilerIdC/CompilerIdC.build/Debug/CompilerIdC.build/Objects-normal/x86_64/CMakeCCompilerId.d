8f3e87a6a6c8aaf6639ff8154a2e9ebb2ed65f78
Foti Dim <fdimanidis@gmail.com>
2019-08-17 21:55:18 +0200

    Add Xcode project

---
+++ CMakeFiles/3.15.2/CompilerIdC/CompilerIdC.build/Debug/CompilerIdC.build/Objects-normal/x86_64/CMakeCCompilerId.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/fotidim/Dev/CarND-Extended-Kalman-Filter-Project/CMakeFiles/3.15.2/CompilerIdC/CMakeCCompilerId.c
---
+++ ExtendedKF.build/Debug/ZERO_CHECK.build/Script-402951ACD00B40E890F58823.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+make -C /Users/fotidim/Dev/CarND-Extended-Kalman-Filter-Project -f /Users/fotidim/Dev/CarND-Extended-Kalman-Filter-Project/CMakeScripts/ZERO_CHECK_cmakeRulesBuildPhase.make$CONFIGURATION OBJDIR=$(basename "$OBJECT_FILE_DIR_normal") all
